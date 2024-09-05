c580e2728aff4aea2ffa907b9b904655c8ef2024
Ashutosh-00-ds <ashutoshgautam2003@gmail.com>
2024-09-06 02:58:57 +0530

    progrram to add i'ntersection of two list

--- index.py
+++ index.py
@@ -1,15 +1,15 @@
 # maximum profit in trading stock
 
-'''prices = [9, 1, 3, 6, 4, 8, 3, 5, 5] 
+prices = [9, 1, 3, 6, 4, 8, 3, 5, 5] 
 min_price=prices[0]
 max_profit=0
 for i in prices:
     max_profit=max(max_profit,i-min_price)
     min_price=min(min_price,i)
-print('maximum profit: ',max_profit)'''
+print('maximum profit: ',max_profit)
 
 # factorial zeroes trail
-'''def factorial(n):
+def factorial(n):
     a=0
     b=5
     while n>=b:
@@ -17,7 +17,7 @@ print('maximum profit: ',max_profit)'''
         b*=5
     return ('number of zeroes in factoral:',a)
 print(factorial(5))
-'''
+
 
 # find missing number in list
 
@@ -27,4 +27,13 @@ c=len(input_set)+int(input('how many missiing number you want to find: '))
 for i in range (c):
     if i  not in  input_set:
         print(i)
+        
+# intersection of two list
+a=[1,2,3,4]
+b=[2,4,5,6]
+d=[]
+c=set(a)&set(b)
+for i in c:
+    d.append(i)
+print(d)
 
